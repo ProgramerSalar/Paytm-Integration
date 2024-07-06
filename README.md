125729b480b25ff5237e0faeb87c2994556406e6
Manish Kumar <95423130+ProgramerSalar@users.noreply.github.com>
2024-07-06 21:34:09 +0530

    payment verification

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reqzor pay integration
+
+
+i follow this documentation: https://axisbank-docs.razorpay.com/payments/server-integration/nodejs/
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -8,11 +8,38 @@ const Home = () => {
 
     const checkoutHandler = async (amount) => {
 
-        // const {data} = await axios.post("http://localhost:4000/api/checkout",{
-        //   amount
-        // })
+      const {data:{key}} = await axios.get("http://localhost:4000/api/getkey")
+
+        const {data:{order}} = await axios.post("http://localhost:4000/api/checkout",{
+          amount
+        })
         // console.log(data)
         // console.log(window)
+
+        const options = {
+          "key": key, // Enter the Key ID generated from the Dashboard
+          "amount": order.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+          "currency": "INR",
+          "name": "Manish Kumar",
+          "description": "Tutorial of RezorPay",
+          "image": "https://avatars.githubusercontent.com/u/95423130?s=400&u=c3cfd05f71ec0e43cf86f3cd3b3f86abd67fdc53&v=4",
+          "order_id": order.id, //This is a sample Order ID. Pass the `id` obtained in the previous step
+          "callback_url":"http://localhost:4000/api/paymentVerification",
+          "prefill": {
+              "name": "Gaurav Kumar",
+              "email": "gaurav.kumar@example.com",
+              "contact": "9999999999"
+          },
+          "notes": {
+              "address": "Razorpay Corporate Office"
+          },
+          "theme": {
+              "color": "#3399cc"
+          }
+      };
+      const razor = new window.Razorpay(options);
+      razor.open()
+    
 
     }
   return (
