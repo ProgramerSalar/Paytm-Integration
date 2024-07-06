ba15491945b0c0f35a8160511a577877d82da131
Manish Kumar <95423130+ProgramerSalar@users.noreply.github.com>
2024-07-06 22:06:04 +0530

    total payment successfull

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './page/Home'
+import PaymentSuccess from './page/PaymentSuccess'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/paymentsuccess'  element={<PaymentSuccess/>}/>
       </Routes>
 
     </Router>
